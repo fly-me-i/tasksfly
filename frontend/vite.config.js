bccfd3be0e811dad53209c1e6bae0f211eb57201
fly-me-i <omprakashsahuc@gmail.com>
2026-08-05 19:41:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // See https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tasksfly",
+  base: "/tasksfly/",
   server: {
     port: 5173,
   },
